1c4283f21d7eef07acd28683d56e09501dc3ce66
佐藤力斗 <riki@riki.local>
2025-05-19 12:07:46 +0900

    コマンドラインでファイルを指定するように改良

--- src/rcat.rb
+++ src/rcat.rb
@@ -1,6 +1,8 @@
-# reader.rb の各行を1行ずつ画面に表示
-File.open("reader.rb", "r") do |f|
-  while line = f.gets&.chomp
-    puts line
+# コマンドラインで指定した全てのファイルの各行を1行ずつ画面に表示
+ARGV.each do |arg|
+  File.open arg, "r" do |f|
+    while line = f.gets&.chomp
+      puts line
+    end
   end
 end
