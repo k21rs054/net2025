dec8afa3f992133108a288eff8d2406bc3b04436
佐藤力斗 <riki@riki.local>
2025-06-09 11:51:43 +0900

    ヘッダ情報非表示

--- src/client1.rb
+++ src/client1.rb
@@ -3,6 +3,8 @@
 # ./client1.rb ホスト名 パス名
 # ruby client1.rb ホスト名 パス名
 
+# ヘッダー情報を表示しないように改良
+
 require "socket"
 
 # host,path = ARGV
@@ -15,6 +17,14 @@ s.print("\r\n")
 # cmd = "GET #{ARGV[1]} HTTP/1.1\r\n"
 # cmd += "Host: #{ARGV[0]}\r\n"
 # cmd += "\r\n"
+
+while line = s.gets&.chomp
+  break if line.empty?
+  # if line == "\r\n"
+  #   break
+  # end
+  # puts line
+end
 
 while line = s.gets&.chomp
   puts line
