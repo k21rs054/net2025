1e31bda44c6e87fe1385f0d428c48c4d5c7db4b5
佐藤力斗 <riki@riki.local>
2025-06-30 12:38:28 +0900

    HTTP1.0に対応

--- src/httpserver1.rb
+++ src/httpserver1.rb
@@ -12,15 +12,36 @@ s0 = TCPServer.open(80)
 loop do
   sock = s0.accept
   Thread.new do
-    while line = sock.gets
-      print line
-      sock.print line
-      if line == "\r\n"
-        break
+    line = sock.gets&.chomp
+    _cmd,path,_ver = line.split
+    # a = line.split
+    # path = a[1]
+    case path
+    when "/hello"
+      sock.puts "HTTP/1.0 200 OK"
+      sock.puts
+      sock.puts "Hello, world!"
+    when "/93"
+      sock.puts "HTTP/1.0 301 Moved Permanently"
+      sock.puts "Location: http://www.kyusan-u.ac.jp"
+      sock.puts
+    else
+      # path = ".#{path}"
+      if File.exist?("." + path)
+        sock.puts "HTTP/1.0 200 OK"
+        sock.puts "Content-Type: text/plane; charset=UTF-8"
+        sock.puts
+        File.open "." + path, "r" do |f|
+          while fl = f.gets&.chomp
+            sock.puts fl
+          end
+        end
+      else
+        sock.puts "HTTP/1.0 404 Not Found"
+        sock.puts
       end
     end
     sock.close
-    sleep 60
   end
 end
 
