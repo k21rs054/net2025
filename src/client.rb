41b0307a91fa50eb0ece2523546313b51820ef9b
佐藤力斗 <riki@riki-2.local>
2025-07-24 20:23:51 +0900

    一旦完成

--- src/client.rb
+++ src/client.rb
@@ -1,1 +1,37 @@
-#!/usr/bin/ruby
+#! /usr/local/bin/ruby
+
+require "socket"
+
+print "ユーザ名: "
+username = gets.chomp
+
+puts "終了するときは \"exit\" と入力してください"
+
+sock = TCPSocket.open("localhost", 80)
+sock.puts username  # ユーザ名の送信
+
+# メッセージ受信
+Thread.new do
+  loop do
+    message = sock.gets
+    if message == nil
+      break 
+    end
+    puts "\n#{message.chomp}"
+    print "> "
+  end
+end
+
+# メッセージ送信
+loop do
+  print "> "
+  input = gets.chomp
+  # "exit"と入力したら接続を切る
+  if input == "exit"
+    break
+  end
+  sock.puts input
+end
+
+sock.close
+puts "接続を終了しました"
--- src/server.rb
+++ src/server.rb
@@ -1,1 +1,55 @@
 #!/usr/bin/ruby
+
+require "socket"
+
+# []: 配列
+# {}: ハッシュ
+clients = [] # クライアント一覧
+usernames = {} # ユーザー名の対応表
+server = TCPServer.open(80)
+
+loop do
+  sock = server.accept
+  clients << sock
+
+  Thread.new(sock) do |client|
+    begin
+      # 最初にユーザー名を受信（1行目）
+      name_line = client.gets
+      username = name_line.chomp
+      usernames[client] = username
+      puts "#{username} が参加しました"
+
+      # 他のクライアントに通知
+      clients.each do |c|
+        if c != client
+          c.puts "通知: #{username} が参加しました"
+        end
+      end
+
+      while message = client.gets
+        message&.chomp
+        puts "#{username}: #{message}"
+
+        # 他のクライアントにメッセージを転送
+        clients.each do |c|
+          if c != client
+            c.puts "#{username}: #{message}"
+          end
+        end
+      end
+
+    ensure # 最後に実行 (切断)
+      clients.delete(client)
+      name = usernames.delete(client)
+      puts "#{name} が切断しました"
+
+      # 他のクライアントに退出の通知
+      clients.each do |c|
+        c.puts "通知: #{name} が退出しました"
+      end
+
+      client.close
+    end
+  end
+end
